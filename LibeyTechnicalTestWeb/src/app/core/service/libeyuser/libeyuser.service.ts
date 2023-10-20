3144ebc31adc3d14e780be3736fb763e2c631bae
georgeSaT123 <georgest2596@gmail.com>
2023-10-20 07:49:53 -0500

    update: methods of service, initial

--- LibeyTechnicalTestWeb/src/app/core/service/libeyuser/libeyuser.service.ts
+++ LibeyTechnicalTestWeb/src/app/core/service/libeyuser/libeyuser.service.ts
@@ -3,13 +3,24 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { environment } from "../../../../environments/environment";
 import { LibeyUser } from "src/app/entities/libeyuser";
+import { LibeyUserCommand } from "src/app/entities/Request/libeyusercommand";
 @Injectable({
 	providedIn: "root",
 })
 export class LibeyUserService {
 	constructor(private http: HttpClient) {}
-	Find(documentNumber: string): Observable<LibeyUser> {
+
+	public Find(documentNumber: string): Observable<LibeyUser> {
 		const uri = `${environment.pathLibeyTechnicalTest}LibeyUser/${documentNumber}`;
 		return this.http.get<LibeyUser>(uri);
 	}
+
+	public getAllLibeyUsers(): Observable<LibeyUser[]> {
+		const uri = `${environment.pathLibeyTechnicalTest}LibeyUser/all`;
+		return this.http.get<LibeyUser[]>(uri);
+	}
+	public createUser(command: LibeyUserCommand): Observable<any> {
+		const uri = `${environment.pathLibeyTechnicalTest}LibeyUser`;
+		return this.http.post(uri, command);
+	}	  
 }
