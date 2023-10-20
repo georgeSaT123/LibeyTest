19415907e96087627134c0e12b21abd2b44076a5
georgeSaT123 <georgest2596@gmail.com>
2023-10-20 07:51:20 -0500

    create services of documentType, province, region, and ubigeo

---
+++ LibeyTechnicalTestWeb/src/app/entities/Request/libeyusercommand.ts
@@ -0,0 +1,12 @@
+export interface LibeyUserCommand{
+    documentNumber:string;
+    documentTypeId:number;
+    name:string;
+    fathersLastName :string;
+    mothersLastName :string;
+    address :string;
+    ubigeoCode :string;
+    phone :string;
+    email :string;
+    password :string;
+}
